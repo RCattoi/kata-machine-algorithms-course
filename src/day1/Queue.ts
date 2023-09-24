d71737fd6a59a036c4b40463b41a495e5f3deaae
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-09-23 21:33:18 -0300

    feat queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+      this.head = this.tail = undefined;
+      this.length = 0
+
     }
 
     enqueue(item: T): void {
+      this.length++
+      const newTail = {value: item} as Node<T>
+      if (!this.tail){
+        this.tail = this.head = newTail
+      }
+      
+      this.tail.next = newTail
+      this.tail = newTail
 
-}
+
+    }
     deque(): T | undefined {
+      if (!this.head) {
+        return undefined;
+      }
 
+      this.length--
+
+      const head = this.head
+      this.head = this.head.next
+
+      return head.value
 }
     peek(): T | undefined {
-
+      return this.head?.value
 }
 }
