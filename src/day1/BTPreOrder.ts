9290c8bc81c3b3c77291632bf9de0e2eaff887e9
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-09-28 15:51:27 -0300

    feat: binary tree

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,19 @@
+function walk(curr: BinaryNode<number> | null, path:number[]): number[] {
+  if (!curr) {
+    return path;
+  }
+  // pre
+  path.push(curr.value)
+
+  // recurse
+  walk(curr.left, path)
+  walk(curr.right, path)
+// pos
+  return path
+
+}
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+  
+  return walk(head,[])
 
 }
