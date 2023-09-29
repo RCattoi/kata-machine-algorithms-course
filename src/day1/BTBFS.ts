2fd965d2e771fa1d7f0a1d5e1d51321b3561e84c
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-09-29 15:05:17 -0300

    feat: breadth search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<Number> | null)[] = [head];
 
+  while (q.length) {
+    
+    const curr = q.shift() as BinaryNode<Number> | undefined | null;
+    if (!curr) {
+      continue;
+    }
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    q.push(curr.left)
+    q.push(curr.right)
+  
+  }
+
+  return false
 }
